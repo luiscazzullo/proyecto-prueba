b1398f68e181e1cec8e8b531fb56e6e9dbafd04e
Luis Cazzullo <lcazzullo@manta.com>
2021-06-16 20:03:09 -0300

    config inicial

---
+++ js/app.js
@@ -0,0 +1,3 @@
+//Javascript principal del proyecto
+const game = new Games('Age of empire', 'Juego de estrategia', 'Estrategia');
+console.log(game);
---
+++ js/games.js
@@ -0,0 +1,6 @@
+function Games(title, description, category) {
+  this.title = title;
+  this.description = description;
+  this.category = category;
+}
+
